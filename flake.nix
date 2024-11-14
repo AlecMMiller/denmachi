310ab134250bb8a7660faf117f86c8220a66bf71
Alec Miller <alecmichaelmiller@gmail.com>
2024-11-14 06:39:01 -0700

    initial webpage

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        packages = with pkgs; [
+          yarn
+          nodejs
+        ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+        };
+      }
+    );
+}
